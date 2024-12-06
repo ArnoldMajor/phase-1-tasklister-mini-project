ae155b7b215698325152f496c1164b0ef8a31cf8
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2024-12-06 15:55:26 +0300

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById('create-task-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputField = document.getElementById('new-task-description');
+    const todoList = document.getElementById('tasks');
+    const taskItem = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+
+    todoList.appendChild(taskItem);
+    taskItem.textContent = `${inputField.value} `;
+    taskItem.appendChild(deleteBtn);
+    deleteBtn.textContent = 'X';
+
+    deleteBtn.addEventListener('click', () => {
+      taskItem.remove();
+    })
+  });
 });
